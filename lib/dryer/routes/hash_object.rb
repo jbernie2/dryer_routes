52f7aa255fa99e6ee4b4719e8a5b21b712ff886f
John Bernier <john.bernier@stash.com>
2024-01-24 16:58:13 -0500

    Allow url to be accessed correctly from route registry

--- lib/dryer/routes/hash_object.rb
+++ lib/dryer/routes/hash_object.rb
@@ -1,16 +1,25 @@
+require 'dryer_services'
+
 module Dryer
   module Routes
-    class HashObject
-     def initialize(hash)
+    class HashObject < Dryer::Services::SimpleService
+      def initialize(hash)
         hash.each do |k,v|
           key = k.is_a?(Numeric) ? "_#{k}" : k
 
-          self.instance_variable_set(
-            "@#{key}", v.is_a?(Hash) ? HashObject.new(v) : v
-          )
-          self.class.send(
-            :define_method, key, proc{self.instance_variable_get("@#{key}")}
-          )
+          if v.is_a?(Proc)
+            self.send(:define_singleton_method, key, proc do |*args, **kwargs, &block|
+              v.arity != 0 ? v.call(*args, **kwargs, &block) : v.call
+            end)
+          else
+            self.instance_variable_set(
+              "@#{key}", v.is_a?(Hash) ? HashObject.new(v) : v
+            )
+
+            self.send(
+              :define_singleton_method, key, proc{self.instance_variable_get("@#{key}")}
+            )
+          end
         end
       end
     end
